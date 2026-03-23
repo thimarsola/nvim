4997047e1e23d172dc770bc1eb717f1d700a01ab
Thiago Marsola <36829328+thimarsola@users.noreply.github.com>
2026-03-23 11:27:18 -0300

    refactor(colorsheme.lua): replace pinnord colorscheme with solarized and adjust configuration The colorscheme is changed from pinnord to solarized to provide a different visual experience. The configuration for the transparent background is removed, and the new colorscheme is set in the light mode function, ensuring a consistent appearance across different modes. This enhances the overall aesthetics and usability of the editor.

--- lua/v1/plugins/colorsheme.lua
+++ lua/v1/plugins/colorsheme.lua
@@ -3,18 +3,16 @@ return {
     "r2luna/pinnord.nvim",
     enabled = true,
     priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme pinnord]])
-
-      -- Transparent background only in dark mode
-      if vim.o.background == "dark" then
-        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
-      end
-    end,
+  },
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = {
+        enabled = false,
+      },
+    },
   },
   {
     "f-person/auto-dark-mode.nvim",
@@ -32,7 +30,7 @@ return {
       end,
       set_light_mode = function()
         vim.o.background = "light"
-        vim.cmd("colorscheme pinnord")
+        vim.cmd("colorscheme solarized")
       end,
     },
   },
